5cff8c0b50bf3b044f3252830430fb5abcce274f
lgq5201314 <lgq97@foxmail.com>
2022-07-06 17:06:00 +0800

    添加vue-router,vuex,常用的一些weigets

---
+++ src/components/mapTool.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <div class="tool-panel">
+            <ul class="tool">
+                <li>
+                    行政区导航
+                    
+                </li>
+                <li><span>|</span></li>
+                <li>
+                    距离测量
+                </li>
+               <li><span>|</span></li>
+                <li>
+                    面积测量
+                </li>
+                <li><span>|</span></li>
+                <li>
+                    清除
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'mapTool',
+};
+</script>
+
+<style>
+.tool-panel .tool {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    width: 303px;
+    height: 40px;
+    background: white;
+    display: flex;
+    
+}
+.tool-panel .tool li {
+    flex-grow: 1;
+    line-height: 40px;
+    text-align: center;
+}
+.tool li:hover {
+    background: grey;
+    cursor:pointer
+}
+.tool-panel .tool span{
+    display: block;
+    line-height: 40px;
+   
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,19 @@
-import vue from "vue"
-import VueRouter from "vue-router"
+import vue from "vue";
+import VueRouter from "vue-router";
+import oneMap from '../pages/oneMap';
+import homeScreen from "../pages/homeScreen"
 // 使用插件
 vue.use(VueRouter);
 // 配置路由
 export default new VueRouter({
-
+    routes:[
+        {
+            path:'/',
+            component:homeScreen
+        },
+        {
+            path:'/oneMap',
+            component:oneMap
+        }
+    ]
 })
